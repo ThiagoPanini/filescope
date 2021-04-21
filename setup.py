4feeb280d683fe24751590677fd9fc713e18fd2b
ThiagoPanini <thipanini94@gmail.com>
2021-04-21 17:04:38 -0300

    Release versao 0.0.6

--- setup.py
+++ setup.py
@@ -37,13 +37,13 @@ reqs = [str(ir.req) for ir in install_reqs]"""
 # Criando setup
 setup(
     name='filescope',
-    version='0.0.5',
+    version='0.0.6',
     author='Thiago Panini',
     author_email='thipanini94@gmail.com',
     packages=find_packages(),
     install_requires=[
         'pandas==1.1.5',
-        'matplotlib==3.2.1'
+        'matplotlib==3.2.1',
         'seaborn==0.11.1',
     ],
     license='MIT',
